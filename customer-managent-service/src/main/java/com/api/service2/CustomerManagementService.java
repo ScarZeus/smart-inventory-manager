a7576da22fb6fefc059bed468de34e48d848116b
ScarZeus <kevingladson2427@gmail.com>
2026-04-11 21:51:54 +0530

    Dependency Caching is Going on

--- customer-managent-service/src/main/java/com/api/service2/CustomerManagementService.java
+++ customer-managent-service/src/main/java/com/api/service2/CustomerManagementService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Service2Application {
+public class CustomerManagementService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Service2Application.class, args);
+		SpringApplication.run(CustomerManagementService.class, args);
 	}
 
 }
